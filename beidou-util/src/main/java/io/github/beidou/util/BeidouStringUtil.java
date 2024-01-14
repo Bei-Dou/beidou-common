f3e4c7a58492401276ef73dfb5182c12b53a3899
bihaiyang <2217704900@qq.com>
2024-01-14 22:56:34 +0800

    update group

--- beidou-util/src/main/java/io/github/beidou/util/BeidouStringUtil.java
+++
@@ -1,9 +0,0 @@
-package io.github.beidou.util;
-
-/**
- * @author bihaiyang
- * @date 2024/1/14 15:41
- */
-public class BeidouStringUtil {
-
-}
---
+++ beidou-util/src/main/java/io/github/beidou/util/StringUtil.java
@@ -0,0 +1,12 @@
+package io.github.beidou.util;
+
+/**
+ * @author bihaiyang
+ * @date 2024/1/14 15:41
+ */
+public class StringUtil {
+
+  public Boolean isEmpty(String str) {
+    return str == null || str.isEmpty();
+  }
+}
